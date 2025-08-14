f20352fe17c269201c2d98565f2f814e927c5337
Reid Vanvliet <Reidvanvliet56@hotmail.com>
2025-08-14 14:23:20 -0700

    configure proxy server to look like a browser request

--- netlify/functions/reddit-proxy.js
+++ netlify/functions/reddit-proxy.js
@@ -29,7 +29,13 @@ exports.handler = async (event, context) => {
     
     const response = await fetch(url.toString(), {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (compatible; Reid-It/1.0)'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
       }
     });
     
